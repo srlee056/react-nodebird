ec6abd20bb516dd20ea93469b6b93289a37fcf1d
Lee Sarah <imsolem1226@gmail.com>
2021-07-02 17:14:16 +0900

    install modules and additional settings
    node, react, next
    eslint

---
+++ prepare/front/pages/about/seorim.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Seorim = () => {
+  return <div>이서림</div>;
+};
+
+export default Seorim;
---
+++ prepare/front/pages/index.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+// next ㅇㅔ서는 이 부분 필요가 없음.
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
