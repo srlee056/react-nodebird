ddac7bbb1feb695911dff6ac0d547441c2f20ef1
Lee Sarah <imsolem1226@gmail.com>
2021-07-02 17:27:59 +0900

    install antd and styled componenets

--- prepare/front/.next/static/chunks/webpack.js
+++ prepare/front/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "55ac5b877907c5ffe825";
+/******/ 	var hotCurrentHash = "b86d00eb1f648aea46e5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,20 +1,29 @@
 import React from "react";
 import propTypes from "prop-types";
 import Link from "next/link";
+import { Menu } from 'antd';
+import 'antd/dist/antd.css';
+
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
+      <Menu mode = "horizontal">
+        <Menu.Item>
         <Link href="/">
           <a>노드버드</a>
         </Link>
+        </Menu.Item>
+        <Menu.Item>
         <Link href="/profile">
           <a>프로필</a>
         </Link>
+        </Menu.Item>
+        <Menu.Item>
         <Link href="/signup">
           <a>회원가입</a>
         </Link>
-      </div>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
